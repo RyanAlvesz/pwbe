209d361ccf790116befa1d7a15cb6dfb9175c834
Ryan Alves <ra546038@gmail.com>
2023-09-15 12:02:21 -0300

    AULA - 01

---
+++ AULA - 01/app.js
@@ -0,0 +1,18 @@
+// Permite exibir uma mensagem no terminal
+console.log('Testando o Node.JS');
+
+// Criando variável
+var nome = 'Ryan';
+var numero = 10;
+var booleano = true;
+var matriz = [];
+
+// Para concatenar podemos utilizar + ou a forma reduzida com ` e ${}
+console.log("O nome do usuário é: " + nome);
+console.log(`Eu tenho ${numero} anos`)
+
+// typeof() permite identificar qual o tipo de dados de uma variável ou objeto
+console.log(typeof(nome));
+console.log(typeof(numero));
+console.log(typeof(booleano));
+console.log(typeof(matriz));
