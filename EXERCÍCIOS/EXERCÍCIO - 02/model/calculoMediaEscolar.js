9ea856cf7358311849e95f4195a18c8d34f3c41a
Ryan Alves <ra546038@gmail.com>
2023-09-15 12:03:13 -0300

    EXERCÍCIO - 02

---
+++ EXERCÍCIOS/EXERCÍCIO - 02/model/calculoMediaEscolar.js
@@ -0,0 +1,38 @@
+/****************************************************************************************************************************************************
+* Objetivo: Arquivo de funções para calcular a média escolar
+* Data: 15/09/2023
+* Autor: Ryan Alves
+* Versão: 1.0
+****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************
+* Nome do aluno; 
+* Nome do professor; 
+* Sexo do professor e do aluno; 
+* Nome do curso; 
+* Nome da disciplina; 
+* 4 Notas que podem ser entre 0 e 100;
+****************************************************************************************************************************************************/
+
+const calcularMedia = (valorNota1, valorNota2, valorNota3, valorNota4) => {
+
+    
+
+}
+
+console.log(calcularMedia())
+
+/*************
+let aluno = nomeAluno, professor = nomeProfessor, pronomeAluno = sexoAluno, pronomeProfessor = sexoProfessor
+let curso = nomeCurso, disciplina = nomeDisciplina
+let nota1 = valorNota1, nota2 = valorNota2, nota3 = valorNota3, nota4 = valorNota4
+
+if(aluno == '' || professor == '' || pronomeAluno == '' || pronomeProfessor == '' || curso == '' || disciplina == '' || nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' ){
+    console.log('ERRO: Todos os valores devem ser preenchidos')
+}else if (nota1 > 100 || nota1 < 0 || nota2 > 100 || nota2 < 0 || nota3 > 100 || nota3 < 0 || nota4 > 100 || nota4 < 0)
+    console.log('ERRO: As notas devem estar entre 0 e 100') 
+**************/
+
+module.exports = {
+    calcularMedia    
+}
