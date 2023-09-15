6877255ff2965e362b1d1060ac035a38419ef414
Ryan Alves <ra546038@gmail.com>
2023-09-15 12:24:44 -0300

    EXERCÍCIO - 02

--- EXERCÍCIOS/EXERCÍCIO - 02/model/calculoMediaEscolar.js
+++ EXERCÍCIOS/EXERCÍCIO - 02/model/calculoMediaEscolar.js
@@ -16,23 +16,62 @@
 
 const calcularMedia = (valorNota1, valorNota2, valorNota3, valorNota4) => {
 
-    
+    let nota1 = valorNota1, nota2 = valorNota2, nota3 = valorNota3, nota4 = valorNota4, media
+
+    if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' )
+        console.log('ERRO: Todos os valores devem ser preenchidos')
+    if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4))
+        console.log('ERRO: Todos os valores devem ser números')
+    else if (nota1 > 100 || nota1 < 0 || nota2 > 100 || nota2 < 0 || nota3 > 100 || nota3 < 0 || nota4 > 100 || nota4 < 0)
+        console.log('ERRO: As notas devem estar entre 0 e 100') 
+    else{
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4
+
+    }
+
+    if(media != undefined)
+        return media
+    else
+        return false
 
 }
 
-console.log(calcularMedia())
+const verificarSituacao = (valorMedia) => {
+
+    let media = valorMedia, situacao
+
+    if(media >= 70)
+        situacao = 'APROVADO'
+    else 
+        situacao = 'REPROVADO'
+
+    return situacao
+
+}
+
+const exame = (valorExame, valorMedia) => {
+
+    let notaExame = valorExame, novaMedia, valorMedia
+
+    novaMedia = (notaExame + valorMedia) / 2 
+
+    return novaMedia
+
+}
+
+console.log()
 
 /*************
 let aluno = nomeAluno, professor = nomeProfessor, pronomeAluno = sexoAluno, pronomeProfessor = sexoProfessor
 let curso = nomeCurso, disciplina = nomeDisciplina
-let nota1 = valorNota1, nota2 = valorNota2, nota3 = valorNota3, nota4 = valorNota4
 
-if(aluno == '' || professor == '' || pronomeAluno == '' || pronomeProfessor == '' || curso == '' || disciplina == '' || nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' ){
+if(aluno == '' || professor == '' || pronomeAluno == '' || pronomeProfessor == '' || curso == '' || disciplina == ''){
     console.log('ERRO: Todos os valores devem ser preenchidos')
-}else if (nota1 > 100 || nota1 < 0 || nota2 > 100 || nota2 < 0 || nota3 > 100 || nota3 < 0 || nota4 > 100 || nota4 < 0)
-    console.log('ERRO: As notas devem estar entre 0 e 100') 
+
 **************/
 
 module.exports = {
-    calcularMedia    
+    calcularMedia,
+    verificarSituacao
 }
